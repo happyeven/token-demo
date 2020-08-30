b38f2f407ed859ea7f2112bf715d6b349a00d557
happyeven <1361840595@qq.com>
2020-08-31 00:18:59 +0800

    [token - demo] [dong] init :　init the demo

--- src/main/java/com/zhu/controller/FirstController.java
+++ src/main/java/com/zhu/controller/FirstController.java
@@ -1,18 +1,35 @@
 package com.zhu.controller;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.UUID;
 
+import com.zhu.util.CookieUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class FirstController {
-	
-	@RequestMapping("/show")
-	public String showView(Model model) {
-		model.addAttribute("msg", "first work");
-		model.addAttribute("key", new Date());
-		return "index";
+
+
+	@RequestMapping("/login")
+	public String doLogin() {
+		return "login";
 	}
+
+	@RequestMapping("/getToken")
+	public void getToken(String username , String password , HttpServletRequest request , HttpServletResponse response) throws IOException {
+		System.out.println(username + password);
+		String token = UUID.randomUUID().toString();
+		CookieUtils.setCookie(request,response,"token" ,token);
+		response.sendRedirect("success");
+	}
+
+
 }
